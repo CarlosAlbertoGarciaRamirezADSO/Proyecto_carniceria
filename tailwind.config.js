ad572657b70f72992f746de76d48b2b3c2f583d1
CarlosAlbertoGarciaRamirezADSO <calbertogarciaramirez123@gmail.com>
2024-05-10 01:06:00 -0500

    Registro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
-tailwind.config = {
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config = {
+//   theme: {
+//     extend: {
+//       colors: {
+//         clifford: '#da373d',
+//         bag_white: '#000',
+//         color_carne: '#a52a2a',
+//       }
+//     }
+//   }
+// }
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         clifford: '#da373d',
         bag_white: '#000',
         color_carne: '#a52a2a',
+        fondo:'#e5e5e5'
       }
     }
-  }
+  },
+  plugins: [],
 }

14e2f0a9775241307e53ba054a64ea50fb3aa18c
Donijimg <>
2024-06-07 18:47:27 -0500

    ejercicios

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,29 +7,53 @@
 //   plugins: [],
 // }
 
-// tailwind.config = {
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        bag_white: '#ffffff',
+        // font: "Oswald", sans - serif;,
+        hover_btn: '#f73535',
+        color_carne: '#a52a2a',
+        color_fondo: '#e5e5e5',
+        rojo_claro: '#f73535',
+        rojo_oscuro: '#a52a2a',
+        gris: '#e5e5e5',
+        gm: '#808080',
+        go: '#404040',
+        negro: '#000000',
+        inter:'#c94242'
+      }
+    }
+  }
+}
+
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
 //   theme: {
 //     extend: {
 //       colors: {
 //         clifford: '#da373d',
 //         bag_white: '#000',
 //         color_carne: '#a52a2a',
+//         fondo:'#e5e5e5'
 //       }
 //     }
-//   }
+//   },
+//   plugins: [],
 // }
 
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        clifford: '#da373d',
-        bag_white: '#000',
-        color_carne: '#a52a2a',
-        fondo:'#e5e5e5'
-      }
-    }
-  },
-  plugins: [],
-}
+// module.exports = {
+//   theme: {
+//     fontFamily: {
+//       sans: [
+//         '"Inter var", sans-serif',
+//         {
+//           font_letra: '"Oswald", sans - serif',
+//           fontFeatureSettings: '"cv11", "ss01"',
+//           fontVariationSettings: '"opsz" 32'
+//         },
+//       ],
+//     },
+//   },
+// }

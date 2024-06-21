efc851d53355dc9d4892fdcd675328980c4833ee
Carlos Garcia <Calbertogarciaramirez123@gmail.com>
2024-06-21 11:46:13 -0500

    adelanto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,18 @@ module.exports = {
     extend: {
       colors: {
         bag_white: '#ffffff',
+        bag_black: '#000000',
         hover_btn: '#f73535',
         color_carne: '#a52a2a',
         color_fondo: '#e5e5e5',
         rojo_claro: '#f73535',
         rojo_oscuro: '#a52a2a',
         gris: '#e5e5e5',
-        gm: '#808080',
-        go: '#404040',
-        negro: '#000000',
-        inter: '#c94242'
+        gris_claro: '#808080',
+        gris_oscuro: '#404040',
+        inter: '#c94242',
+        border_rose:'#f43f5e'
+
       }
     },
   },

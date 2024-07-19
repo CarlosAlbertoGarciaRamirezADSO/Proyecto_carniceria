1d68167d6679b4d8e654f140d9ad4d3fc30f74ae
Carlos Garcia <calbertogarciaramirez123@gmail.com>
2024-07-19 14:35:25 -0500

    menu ham

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,9 @@ tailwind.config = {
         gris_claro: '#808080',
         gris_oscuro: '#404040',
         inter: '#c94242',
-        border_rose:'#f43f5e'
+        border_rose:'#f43f5e',
+
+        rojito:"#cc2b2d"
       }
     }
   }
